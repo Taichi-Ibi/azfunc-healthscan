13c550baded24e2b5d7a8093063a94e6bed22c7b
Taichi-Ibi <t.ibi@estyle-inc.jp>
2023-12-30 12:51:58 +0900

    venvが起動中かどうか判別するロジックを追加

--- pip-remake.sh
+++ pip-remake.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# 仮想環境がアクティブかどうかを確認
+if [ -z "$VIRTUAL_ENV" ]; then
+    echo "Error: No active virtual environment found."
+    exit 1
+fi
+
 # packageをinstallし直す
 pip freeze > uninstall.txt
 pip uninstall -y -r uninstall.txt
