455d9cd9560e7d8e2c9086bfaa05115c20acf9af
Taichi-Ibi <t.ibi@estyle-inc.jp>
2023-12-30 13:53:04 +0900

    pip-deps-check.pyを作成

--- pip-deps-check.py
+++ pip-deps-check.py
@@ -2,34 +2,56 @@ import logging
 import os
 import re
 import subprocess
+import sys
 
 PATH = "requirements.txt"
 
 
-def check_requirements_with_pip_show(requirements_path):
-    # requirements.txt ファイルを読み込む
-    with open(requirements_path, "r") as file:
-        lines = set(line.strip() for line in file if line.strip())
-    required_packages = set(
-        line.split("==")[0] for line in lines if not line.startswith("#")
+def simple_progress_bar(current, total, bar_length=50):
+    fraction = current / total
+    arrow = int(fraction * bar_length - 1) * "-" + ">"
+    padding = int(bar_length - len(arrow)) * " "
+    sys.stdout.write(
+        f"\r\033[34mProgress: [{arrow}{padding}] {int(fraction * 100)}%\033[0m"
     )
+    sys.stdout.flush()
 
-    # 依存関係にあるパッケージをチェックするためのセット
-    dependencies = set()
 
-    # 各パッケージに対して pip show を実行し、依存関係を取得
-    for package in required_packages:
+def get_package_dependencies(package):
+    try:
         output = subprocess.check_output(
             ["pip", "show", package], stderr=subprocess.STDOUT
         ).decode("utf-8")
-        # Requires で始まる行を見つけ、依存関係を抽出
         match = re.search(r"^Requires: (.+)$", output, re.MULTILINE)
         if match:
-            deps = match.group(1).split(", ")
-            dependencies.update(deps)
+            return set(match.group(1).split(", "))
+    except subprocess.CalledProcessError:
+        print()
+        logging.error(f"パッケージ '{package}' が見つかりませんでした。インストールされているか確認してください。")
+        exit(1)
+    return set()
 
-    # requirements.txt にない依存パッケージを識別
-    missing_packages = dependencies - required_packages
+
+def check_requirements_with_pip_show(requirements_path):
+    with open(requirements_path, "r") as file:
+        lines = set(line.strip() for line in file if line.strip())
+    required_packages = [
+        (line.split("==")[0], None) for line in lines if not line.startswith("#")
+    ]
+
+    dependencies = set()
+    total_packages = len(required_packages)
+
+    for i, (package, _) in enumerate(required_packages, 1):
+        deps = get_package_dependencies(package)
+        required_packages[i - 1] = (package, deps)
+        dependencies.update(deps)
+        simple_progress_bar(i, total_packages)
+
+    sys.stdout.write("\n")
+    sys.stdout.flush()
+
+    missing_packages = dependencies - set(pkg for pkg, _ in required_packages)
     if missing_packages:
         message = "依存パッケージの記載漏れが見つかりました。以下のパッケージのインストールを試してみてください。\n{missing_packages}".format(
             missing_packages=os.linesep.join(missing_packages)
@@ -37,5 +59,4 @@ def check_requirements_with_pip_show(requirements_path):
         logging.warning(message)
 
 
-# 依存関係のチェックを実行
 check_requirements_with_pip_show(requirements_path=PATH)
